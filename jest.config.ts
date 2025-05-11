5bb00468430ac4f4307ee8359d3d88339f5abd76
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-05-11 01:51:38 -0300

    first configurations

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+import nextJest from "next/jest";
+import { resolve } from "path";
+
+const root = resolve(__dirname);
+
+const createJestConfig = nextJest({
+  dir: root,
+});
+
+const configJest = {
+  collectCoverage: true,
+  collectCoverageFrom: ["**/src/**/*.ts", "!**/*.gateway.ts", "!**/*.dto.ts"],
+  coverageDirectory: "coverage",
+  coveragePathIgnorePatterns: ["/node_modules/"],
+  setupFilesAfterEnv: ["<rootDir>/src/test/setup/jest.setup.ts"],
+  testEnvironment: "jest-environment-jsdom",
+  testPathIgnorePatterns: ["/node_modules/"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  rootDir: root,
+};
+
+export default createJestConfig(configJest);
