8721cf7ebbf78ea149e96d2d660ba1b54ae4b854
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-05-21 00:39:11 -0300

    feature: created theme file to configure after.

---
+++ src/styles/theme.ts
@@ -0,0 +1,17 @@
+const theme = {
+  colors: {
+    primary: "#00ff00",
+    secondary: "",
+    tertiary: "",
+    quartenary: "",
+  },
+  background: {
+    primary: "#ff0000",
+    secondary: "",
+    tertiary: "",
+    quartenary: "",
+  },
+  border: {},
+} as const;
+
+export { theme };
