9f911de17e5d793d159f45d77a498da58f49b512
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-05-24 22:28:43 -0300

    chore: included css styles into main.

--- src/app/_providers/theme/theme.provider.tsx
+++ src/app/_providers/theme/theme.provider.tsx
@@ -11,10 +11,28 @@ export default function ThemeProvider({
     <main>
       {children}
       <style jsx>{`
+        @keyframes animatedGradient {
+          0% {
+            background-position: 0% 0%;
+          }
+          50% {
+            background-position: 100% 100%;
+          }
+          100% {
+            background-position: 0% 0%;
+          }
+        }
+
         main {
-          width: 100%;
-          height: auto;
-          background: ${theme.background.primary};
+          width: ${theme.width.full};
+          background: ${theme.dark.background.gradient};
+          height: ${theme.height.screen};
+          color: ${theme.dark.colors.primary};
+          background-size: 600% 600%;
+          animation: ${theme.dark.animation.background};
+          font-family: var(--font-quicksand);
+          font-weight: 400;
+          font-size: 1.4rem;
         }
       `}</style>
     </main>
