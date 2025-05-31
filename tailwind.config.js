6037e939aa2c24415e7d5c650f407c8d194d8385
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-05-31 17:57:41 -0300

    feature: create tailwind and postcss configures files.

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/* eslint-disable import/no-anonymous-default-export */
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: ["class"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
