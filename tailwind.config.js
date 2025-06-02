374bde4a5808232655c8696e7125a0ad056f7d18
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-06-02 01:44:24 -0300

    chore: add fill-primary and fill-primary-border

--- tailwind.config.js
+++ tailwind.config.js
@@ -61,6 +61,8 @@ export default {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
+        fillPrimary: "hsl(var(--fill-primary))",
+        fillPrimaryBorder: "hsl(var(--fill-primary-border))",
         chart: {
           1: "hsl(var(--chart-1))",
           2: "hsl(var(--chart-2))",
