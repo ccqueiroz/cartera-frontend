6eeca83afc422a9a27f489621f3a301fce587e9d
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-06-03 12:43:43 -0300

    chore: removed border of the outilne variant button

--- src/components/ui/Button/button.ui.tsx
+++ src/components/ui/Button/button.ui.tsx
@@ -14,7 +14,7 @@ const buttonVariants = cva(mergeClass, {
       destructive:
         "bg-destructive text-destructive-foreground hover:bg-destructive/70",
       outline:
-        "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+        "border-input bg-background hover:bg-accent hover:text-accent-foreground",
       ghost: "hover:bg-accent hover:text-accent-foreground",
       link: "text-primary underline-offset-4 hover:underline",
     },
