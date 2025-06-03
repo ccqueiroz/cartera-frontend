18ca945e2ddabb16f81cc686551d139ae7e30471
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-06-03 05:37:33 -0300

    chore: add Heading into private Layout

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,7 +1,15 @@
+import React from "react";
+import { HeaderPrivate } from "./components/core/Header/index.component";
+
 export default function LayoutPrivateDashboard({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <>{children}</>;
+  return (
+    <div className="w-full h-full flex flex-col gap-4">
+      <HeaderPrivate />
+      {children}
+    </div>
+  );
 }
