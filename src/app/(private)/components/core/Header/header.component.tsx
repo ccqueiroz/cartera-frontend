cc11a224dae6d658c95de0286323e14c423e132d
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-06-05 02:49:38 -0300

    chore: applay Greeting Component inside private header

--- src/app/(private)/components/core/Header/header.component.tsx
+++ src/app/(private)/components/core/Header/header.component.tsx
@@ -2,19 +2,27 @@ import { Avatar } from "../Avatar/avatar.component";
 import { Logo } from "@/app/(public)/components/Logo/logo.component";
 import { cn } from "@/lib/cn.utils";
 import { NotificationsButtons } from "../NotificationsButton/notifications-button.component";
+import { GreetingComponent } from "../GreetingComponent/greeting-component.component";
 
 export function HeaderPrivate() {
   return (
     <header
       aria-label="Header da página"
-      className={cn("flex justify-between items-center p-4 mt-2")}
+      className={cn(
+        "flex flex-col justify-between p-4 mt-2 gap-5"
+      )}
     >
-      <div className="flex items-center [&>div]:mb-0">
-        <Logo />
+      <div className={cn("flex justify-between items-center")}>
+        <div className="flex items-center [&>div]:mb-0">
+          <Logo />
+        </div>
+        <div className="flex items-center gap-5">
+          <NotificationsButtons />
+          <Avatar src="https://avatars.githubusercontent.com/u/61844259?s=96&v=4" />
+        </div>
       </div>
-      <div className="flex items-center gap-5">
-        <NotificationsButtons />
-        <Avatar src="https://avatars.githubusercontent.com/u/61844259?s=96&v=4" />
+      <div>
+        <GreetingComponent />
       </div>
     </header>
   );
