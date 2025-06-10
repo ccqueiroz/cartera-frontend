93eb57006708d920df8a2a6b65d982edcecff08d
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-06-10 20:53:33 -0300

    feature: add nomenclature helpers in index file

---
+++ src/infra/helpers/index.helpers.ts
@@ -0,0 +1,5 @@
+import { ConvertTimestampInDateHelper } from "./convert-timestamp-in-date.infra";
+
+const convertTimeStampInDateHelper = new ConvertTimestampInDateHelper().execute;
+
+export { convertTimeStampInDateHelper };
