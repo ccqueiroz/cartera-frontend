0c165da5cd8a25460a4abbdbf9ecf4e80d4a0323
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-06-12 16:12:09 -0300

    chore: include CardNotFoundData to when dont exist data

--- src/app/(private)/_views/dashboard/BillsPayableMonth/bills-payable-month.view.tsx
+++ src/app/(private)/_views/dashboard/BillsPayableMonth/bills-payable-month.view.tsx
@@ -15,6 +15,7 @@ import { BillCardPayableMonth } from "@/app/(private)/components/dashboard/BillC
 import { DialogHeader } from "@/app/(private)/components/dashboard/DialogHeader/dialog-header.component";
 import { FormBillsPayableMonth } from "@/app/(private)/components/dashboard/FormBillsPayableMonth/form-bills-payable-month.component";
 import { updateBillPayable } from "./bills-payable-month.service";
+import { CardNotFoundData } from "@/app/(private)/components/dashboard/CardFoundData/card-not-found-data.component";
 
 const DialogContent = dynamic(
   () =>
@@ -105,52 +106,56 @@ export default async function BillsPayableMonth() {
       <h2 className="text-lg font-semibold text-white mb-4">
         Contas a Pagar do Mês
       </h2>
-      <div className="space-y-3">
-        {BILLS_DATA.map((bill, index) => (
-          <Dialog key={bill.id}>
-            <DialogTrigger asChild>
-              <BillCardPayableMonth bill={bill} index={index} />
-            </DialogTrigger>
-            <DialogContent>
-              <DialogHeader title="Relatório de Despesa">
-                <span>{bill.descriptionBill}</span>
-                <span className="hidden sm:block">-</span>
-                <span>
-                  Data de vencimento:{" "}
-                  {convertTimeStampInDateHelper(bill.billDate, "DD/MM/AAAA")}
-                </span>
-              </DialogHeader>
-              <form
-                id={`form-update-bill-payable-${bill.id}`}
-                action={updateBillPayable}
-              >
-                <FormBillsPayableMonth bill={bill} />
-              </form>
-              <DialogFooter className="justify-end gap-3 flex-col sm:flex-row">
-                <DialogClose asChild>
-                  <Button
-                    type="button"
-                    variant="destructive"
-                    size="lg"
-                    className="w-full sm:max-w-[120px] text-md"
-                  >
-                    Cancelar
-                  </Button>
-                </DialogClose>
-                <NeonButton
-                  type="submit"
-                  variant="blue"
-                  form={`form-update-bill-payable-${bill.id}`}
-                  size="lg"
-                  className="w-full sm:max-w-[120px]"
+      {BILLS_DATA?.length > 0 ? (
+        <div className="space-y-3">
+          {BILLS_DATA.map((bill, index) => (
+            <Dialog key={bill.id}>
+              <DialogTrigger asChild>
+                <BillCardPayableMonth bill={bill} index={index} />
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader title="Relatório de Despesa">
+                  <span>{bill.descriptionBill}</span>
+                  <span className="hidden sm:block">-</span>
+                  <span>
+                    Data de vencimento:{" "}
+                    {convertTimeStampInDateHelper(bill.billDate, "DD/MM/AAAA")}
+                  </span>
+                </DialogHeader>
+                <form
+                  id={`form-update-bill-payable-${bill.id}`}
+                  action={updateBillPayable}
                 >
-                  Salvar
-                </NeonButton>
-              </DialogFooter>
-            </DialogContent>
-          </Dialog>
-        ))}
-      </div>
+                  <FormBillsPayableMonth bill={bill} />
+                </form>
+                <DialogFooter className="justify-end gap-3 flex-col sm:flex-row">
+                  <DialogClose asChild>
+                    <Button
+                      type="button"
+                      variant="destructive"
+                      size="lg"
+                      className="w-full sm:max-w-[120px] text-md"
+                    >
+                      Cancelar
+                    </Button>
+                  </DialogClose>
+                  <NeonButton
+                    type="submit"
+                    variant="blue"
+                    form={`form-update-bill-payable-${bill.id}`}
+                    size="lg"
+                    className="w-full sm:max-w-[120px]"
+                  >
+                    Salvar
+                  </NeonButton>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
+          ))}
+        </div>
+      ) : (
+        <CardNotFoundData text="Não há contas a serem pagas." variant="dark" />
+      )}
     </GlassCard>
   );
 }
