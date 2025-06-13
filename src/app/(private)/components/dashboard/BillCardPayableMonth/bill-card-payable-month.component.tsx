e88c7522441c809503bc4cc6baa6aa1214568b7b
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-06-12 23:44:58 -0300

    fix: adjust validation when dont exist bill

--- src/app/(private)/components/dashboard/BillCardPayableMonth/bill-card-payable-month.component.tsx
+++ src/app/(private)/components/dashboard/BillCardPayableMonth/bill-card-payable-month.component.tsx
@@ -1,10 +1,10 @@
+import { memo } from "react";
 import { BillsPayableMonthOutPutDTO } from "@/domain/Bill/bill.dto";
 import { BillingCard } from "../BillingCard/billing-card.component";
 import { resolveIcon } from "../../core/IconSlugCategoriesMap/icon-slug-categories-map.component";
-import { convertTimeStampInDateHelper } from "@/infra/helpers/index.helpers";
 import { TextEllipsis } from "../../core/TextEllipsis/text-ellipsis.component";
 import { BadgeStatusPayable } from "../../core/StatusPayable/badge-status-payable.component";
-import { memo } from "react";
+import { convertTimeStampInDateFactory } from "@/factories/infra/convert-timestamp-in-date.infra.factory";
 
 interface BillCardPayableMonthProps {
   bill: BillsPayableMonthOutPutDTO;
@@ -14,30 +14,34 @@ export const BillCardPayableMonth = memo(
   ({ bill, index }: BillCardPayableMonthProps) => {
     return (
       <BillingCard index={index}>
-        {!bill && <p>Sem informações</p>}
-
-        <div className="flex items-center gap-3">
-          <div className="w-10 h-10 rounded-lg flex items-center justify-center glass neon-shadow-blue">
-            {resolveIcon(bill.categoryDescription)}
-          </div>
-          <div>
-            <TextEllipsis text={bill.descriptionBill} maxLength={20} />
-            <p className="text-sm text-white/70">
-              Vence em{" "}
-              {convertTimeStampInDateHelper(bill.billDate, "DD/MM/AAAA")}
-            </p>
-          </div>
-        </div>
-        <div className="w-full sm:w-fit text-right flex sm:flex-col flex-wrap justify-between items-center sm:items-end mt-2 sm:mt-0">
-          <p className="font-semibold digital text-white">
-            {Intl.NumberFormat("pt-Br", {
-              style: "currency",
-              currency: "BRL",
-              minimumFractionDigits: 2,
-            }).format(bill.amount)}
-          </p>
-          <BadgeStatusPayable status={bill.status} />
-        </div>
+        {!bill ? (
+          <p>Sem informações</p>
+        ) : (
+          <>
+            <div className="flex items-center gap-3">
+              <div className="w-10 h-10 rounded-lg flex items-center justify-center glass neon-shadow-blue">
+                {resolveIcon(bill.categoryDescription)}
+              </div>
+              <div>
+                <TextEllipsis text={bill.descriptionBill} maxLength={20} />
+                <p className="text-sm text-white/70">
+                  Vence em{" "}
+                  {convertTimeStampInDateFactory(bill.billDate, "DD/MM/AAAA")}
+                </p>
+              </div>
+            </div>
+            <div className="w-full sm:w-fit text-right flex sm:flex-col flex-wrap justify-between items-center sm:items-end mt-2 sm:mt-0">
+              <p className="font-semibold digital text-white">
+                {Intl.NumberFormat("pt-Br", {
+                  style: "currency",
+                  currency: "BRL",
+                  minimumFractionDigits: 2,
+                }).format(bill.amount)}
+              </p>
+              <BadgeStatusPayable status={bill.status} />
+            </div>
+          </>
+        )}
       </BillingCard>
     );
   }
