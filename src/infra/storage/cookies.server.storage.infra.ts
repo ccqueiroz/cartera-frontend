9cddf84c76a2f50a666c2add44ffdbff07eeb23c
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-06-19 21:46:17 -0300

    fix: remove import to CookieServerStorage non used

--- src/infra/storage/cookies.server.storage.infra.ts
+++ src/infra/storage/cookies.server.storage.infra.ts
@@ -6,16 +6,25 @@ import { CookieOptions } from "@/domain/core/Storage/cookies.dto";
 export class CookieServerStorage implements CookiesGateway {
   constructor(private readonly cookies = nextCookies()) {}
 
+  private parseStringToRequiredType<T>(value: string) {
+    try {
+      const data = JSON.parse(value);
+      return data satisfies T;
+    } catch {
+      return value as string;
+    }
+  }
+
   recover<T>(key: string): T | null {
     const data = this.cookies.get(key);
-
     if (!data || !data?.value) return null;
 
-    return JSON.parse(JSON.stringify(data?.value));
+    return this.parseStringToRequiredType<T>(data.value);
   }
 
   save<T>(key: string, data: T, options?: Partial<CookieOptions>): void {
-    this.cookies.set(key, JSON.stringify(data), options);
+    const value = typeof data === "string" ? data : JSON.stringify(data);
+    this.cookies.set(key, value, options);
   }
 
   delete(key: string): void {
