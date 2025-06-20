e4df5c6efcaa4fff921ca19f2c1635ea37d2b46b
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-06-19 21:25:17 -0300

    chore: convert handleRequestFactory in function, not a singleton.

--- src/factories/infra/handle-request.factory.ts
+++ src/factories/infra/handle-request.factory.ts
@@ -1,5 +1,5 @@
 import { HandleRequestInfra } from "@/infra/api/handle-request.infra";
 
-const handleRequestFactory = new HandleRequestInfra().execute;
+const handleRequestFactory = new HandleRequestInfra();
 
 export { handleRequestFactory };
