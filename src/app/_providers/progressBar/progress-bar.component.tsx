3107b84f43ee1d7d4e87517fdf194f959e54b925
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-06-19 21:19:58 -0300

    feature: create ProgressBar by bprogress/next

---
+++ src/app/_providers/progressBar/progress-bar.component.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { ProgressProvider } from "@bprogress/next/app";
+import "./progress-bar.css";
+
+export const ProgressBar = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ProgressProvider
+      height="4px"
+      color="#7DF9FF"
+      options={{ showSpinner: false }}
+      shallowRouting
+      memo
+      style="background: linear-gradient(90deg, #7DF9FF 0%, #A77DFB 50%, #C084FC 100%);"
+      disableSameURL={false}
+    >
+      {children}
+    </ProgressProvider>
+  );
+};
