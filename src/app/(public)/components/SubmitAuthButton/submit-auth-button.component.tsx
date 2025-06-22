048d917e2898c17ab26b254849b4ac97d62e9ddf
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-06-21 21:24:21 -0300

    refactor: remove hook and transform the button in server component.

--- src/app/(public)/components/SubmitAuthButton/submit-auth-button.component.tsx
+++ src/app/(public)/components/SubmitAuthButton/submit-auth-button.component.tsx
@@ -1,16 +1,19 @@
 import { NeonButton } from "@/components/core/NeonButton/neon-button.component";
-import { useFormStatus } from "react-dom";
 
-export const SubmitAuthButton = ({ title }: { title: string }) => {
-  const { pending } = useFormStatus();
-
+export const SubmitAuthButton = ({
+  title,
+  isSubmitting,
+}: {
+  title: string;
+  isSubmitting: boolean;
+}) => {
   return (
     <div className="w-full max-w-60 mx-auto">
       <NeonButton
         type="submit"
         className="w-full"
-        disabled={pending}
-        isLoading={pending}
+        disabled={isSubmitting}
+        isLoading={isSubmitting}
       >
         {title}
       </NeonButton>
