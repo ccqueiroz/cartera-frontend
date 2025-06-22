8ededad1c0d7b359e3ef938d19406af4045a8eb5
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-06-21 21:24:45 -0300

    chore: add ONLY LETTERS ARE ALLOWED message

--- src/infra/schemas/constants/messages-validations.schema.ts
+++ src/infra/schemas/constants/messages-validations.schema.ts
@@ -3,6 +3,7 @@ export const MESSAGES_SCHEMA_VALIDATIONS = {
   MIN_LENGTH_PASSWORD: "A senha deve conter no mínimo 6 caracteres.",
   MAX_LENGTH_PASSWORD: "",
   INVALID_EMAIL: "E-mail inválido.",
-  confirmPassword_MUST_TO_BE_EQUAL_TO_PASSWORD:
+  CONFIRM_PASSWORD_MUST_TO_BE_EQUAL_TO_PASSWORD:
     "A confirmação de senha deve ser igual à senha.",
+  ONLY_LETTERS_ARE_ALLOWED: 'Apenas letras são permitidas.'
 };
