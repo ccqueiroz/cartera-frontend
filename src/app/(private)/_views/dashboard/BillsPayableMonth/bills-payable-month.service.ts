7d73a262a1cd4dff4b9a3233421da2a58351856e
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-06-23 10:59:42 -0300

    chore: include getBillsPayableMonth in service

--- src/app/(private)/_views/dashboard/BillsPayableMonth/bills-payable-month.service.ts
+++ src/app/(private)/_views/dashboard/BillsPayableMonth/bills-payable-month.service.ts
@@ -1,5 +1,8 @@
 "use server";
 
+import { InputGetBillsPayableMonth } from "@/domain/Bill/bill.dto";
+import { getBillsPayableMonthUseCaseFactory } from "@/factories/usecase/get-bills-payable-month.factory";
+
 export async function updateBillPayable(data: FormData) {
   try {
     const { includePayment, paymentDate } = Object.fromEntries(data.entries());
@@ -8,4 +11,10 @@ export async function updateBillPayable(data: FormData) {
     void paymentDate;
   } finally {
   }
+}
+
+export async function getBillsPayableMonth(queries: InputGetBillsPayableMonth) {
+  const response = await getBillsPayableMonthUseCaseFactory().execute(queries);
+
+  return response;
 }
