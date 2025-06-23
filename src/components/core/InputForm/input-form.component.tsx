4fe69bda396b5c4f4b6274df13d69194d35d8bb1
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-06-23 11:10:06 -0300

    chore: add Input Form like a forward component to received ref

--- src/components/core/InputForm/input-form.component.tsx
+++ src/components/core/InputForm/input-form.component.tsx
@@ -2,6 +2,7 @@ import { Input } from "@/components/ui/Input/input";
 import { Label } from "@/components/ui/Label/label";
 import { InputAlertError } from "../InputAlertError/input-alert-error.component";
 import { cn } from "@/lib/cn.utils";
+import React from "react";
 
 interface InputFormProps extends React.ComponentProps<"input"> {
   id: string;
@@ -12,29 +13,45 @@ interface InputFormProps extends React.ComponentProps<"input"> {
   classNameLabel?: string;
 }
 
-export const InputForm = ({
-  id,
-  error,
-  label,
-  classNameWrapper,
-  classNameInput,
-  classNameLabel,
-  ...props
-}: InputFormProps) => {
-  return (
-    <div
-      className={cn("w-full flex flex-col items-start gap-1", classNameWrapper)}
-    >
-      {label && (
-        <Label
-          htmlFor={id}
-          className={cn("text-sm brightness-75 ml-1", classNameLabel)}
-        >
-          {label}
-        </Label>
-      )}
-      <Input id={id} {...props} className={cn(classNameInput)} />
-      {error && <InputAlertError error={error} />}
-    </div>
-  );
-};
+export const InputForm = React.forwardRef<HTMLInputElement, InputFormProps>(
+  (
+    {
+      id,
+      error,
+      label,
+      classNameWrapper,
+      classNameInput,
+      classNameLabel,
+      ...props
+    },
+    ref
+  ) => {
+    return (
+      <div
+        className={cn(
+          "w-full flex flex-col items-start gap-1",
+          classNameWrapper
+        )}
+      >
+        {label && (
+          <Label
+            htmlFor={id}
+            className={cn("text-sm brightness-75 ml-1", classNameLabel)}
+          >
+            {label}
+          </Label>
+        )}
+        <Input
+          ref={ref}
+          id={id}
+          className={cn(classNameInput)}
+          {...props}
+          aria-invalid={!!error}
+        />
+        {error && <InputAlertError error={error} />}
+      </div>
+    );
+  }
+);
+
+InputForm.displayName = "InputForm";
