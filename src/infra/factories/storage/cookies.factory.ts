6dc7d2bab59945bd6d0a12b9a723d29a6e1096fb
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-07-05 16:43:03 -0300

    fix: fix the cookie factory to create instance of the cookies by next/header and to inject in CookieServerStore class

--- src/infra/factories/storage/cookies.factory.ts
+++ src/infra/factories/storage/cookies.factory.ts
@@ -1,6 +1,10 @@
-import { cookies } from "next/headers";
 import { CookieServerStorage } from "@/infra/storage/cookies.server.storage.infra";
+import { cookies } from "next/headers";
 
-const cookiesFactory = async () => new CookieServerStorage(await cookies());
+const cookiesFactory = async () => {
+  const storage = await cookies();
+
+  return new CookieServerStorage(storage);
+};
 
 export { cookiesFactory };
