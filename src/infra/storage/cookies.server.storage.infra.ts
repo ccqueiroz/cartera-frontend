79b9c7a9b795773317da64c2c6586f088bba3c07
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-07-05 16:46:03 -0300

    fix: remove async/await to when dealing to cookies method and add validation to cookie constructor attribut not found.

--- src/infra/storage/cookies.server.storage.infra.ts
+++ src/infra/storage/cookies.server.storage.infra.ts
@@ -1,9 +1,14 @@
+import { DomainMessageList } from "@/domain/core/constants/domainMessageList.constants";
 import { CookieOptions } from "@/domain/core/storage/cookies.dto";
 import { CookiesGateway } from "@/domain/core/storage/cookies.gateway";
+import { HttpError } from "@/domain/http/http.erro.entitie";
 import { ReadonlyRequestCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies";
 
 export class CookieServerStorage implements CookiesGateway {
-  constructor(private readonly cookies: ReadonlyRequestCookies) {}
+  constructor(private readonly cookies: ReadonlyRequestCookies) {
+    if (!cookies)
+      throw new HttpError(500, DomainMessageList.COOKIES_NOT_DEFINED);
+  }
 
   private parseStringToRequiredType<T>(value: string) {
     try {
@@ -14,23 +19,19 @@ export class CookieServerStorage implements CookiesGateway {
     }
   }
 
-  async recover<T>(key: string): Promise<T | null> {
-    const data = (await this.cookies).get(key);
+  recover<T>(key: string): T | null {
+    const data = this.cookies.get(key);
     if (!data || !data?.value) return null;
 
     return this.parseStringToRequiredType<T>(data.value);
   }
 
-  async save<T>(
-    key: string,
-    data: T,
-    options?: Partial<CookieOptions>
-  ): Promise<void> {
+  save<T>(key: string, data: T, options?: Partial<CookieOptions>): void {
     const value = typeof data === "string" ? data : JSON.stringify(data);
-    (await this.cookies).set(key, value, options);
+    this.cookies.set(key, value, options);
   }
 
-  async delete(key: string): Promise<void> {
-    (await this.cookies).delete(key);
+  delete(key: string): void {
+    this.cookies.delete(key);
   }
 }
