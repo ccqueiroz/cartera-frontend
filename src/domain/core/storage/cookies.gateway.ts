816e567d3295b1efc8b18e53280a13acc1d73d63
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-07-05 16:42:14 -0300

    fix: fix the returns os methods to return not promise

--- src/domain/core/storage/cookies.gateway.ts
+++ src/domain/core/storage/cookies.gateway.ts
@@ -1,7 +1,7 @@
 import { CookieOptions } from "./cookies.dto";
 
 export interface CookiesGateway {
-  recover(key: string): Promise<string | null>;
-  save<T>(key: string, data: T, options?: Partial<CookieOptions>): Promise<void>;
-  delete(key: string | Array<string>): Promise<void>;
+  recover<T>(key: string): T | null;
+  save<T>(key: string, data: T, options?: Partial<CookieOptions>): void;
+  delete(key: string | Array<string>): void;
 }
