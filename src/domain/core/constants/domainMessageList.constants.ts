cd24f197494fd5ed1acb4fe5be3b0af3ffd90e4c
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-07-05 16:41:42 -0300

    chore: include COOKIES_NOT_DEFINED

--- src/domain/core/constants/domainMessageList.constants.ts
+++ src/domain/core/constants/domainMessageList.constants.ts
@@ -4,4 +4,5 @@ export const DomainMessageList = {
   NEXT_PUBLIC_API_URL_NOT_FOUND: "Erro! NEXT_PUBLIC_API_URL não está definida!",
   EMAIL_HAS_BEEN_SEND_TO_RECOVER_PASSWORD:
     "Um e-mail foi enviado para {complement}.",
+  COOKIES_NOT_DEFINED: 'Cookies Store não está definida!'
 } as const;
