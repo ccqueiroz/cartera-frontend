c7953e11938344160d0db072588d5cde835098ff
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-07-05 14:25:14 -0300

    feat: create a http factory

---
+++ src/infra/factories/http/http.factory.ts
@@ -0,0 +1,12 @@
+import { HttpInfra } from "@/infra/http/http.infra";
+import { DomainMessageList } from "@/domain/core/constants/domainMessageList.constants";
+import { CookieServerStorage } from "@/infra/storage/cookies.server.storage.infra";
+
+const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+
+if (!baseUrl) {
+  throw new Error(DomainMessageList.NEXT_PUBLIC_API_URL_NOT_FOUND);
+}
+
+export const httpInfraFactory = () =>
+  new HttpInfra(baseUrl, new CookieServerStorage());
