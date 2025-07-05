64ef8b6aa4ffd1cb2ff58e47ca5c2492c4fdf9ed
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-07-05 00:47:10 -0300

    feat: create ProgressProvider

---
+++ src/app/_providers/progressBar/progressBar.component.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { ProgressProvider } from "@bprogress/next/app";
+
+export const ProgressBar = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ProgressProvider
+      height="4px"
+      color="#7DF9FF"
+      options={{ showSpinner: false }}
+      shallowRouting
+      memo
+      style="background: linear-gradient(90deg, #7DF9FF 0%, #A77DFB 50%, #C084FC 100%);"
+      disableSameURL={false}
+    >
+      {children}
+    </ProgressProvider>
+  );
+};
