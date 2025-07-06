628d70ca5d54b61eef164964fc68f158336598e9
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-07-06 19:47:58 -0300

    feature: create footer view.

--- src/app/(private)/views/footer/footer.view.tsx
+++ src/app/(private)/views/footer/footer.view.tsx
@@ -14,7 +14,6 @@ import {
 } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { memo } from "react";
 
 const menuItems = [
   {
@@ -59,16 +58,16 @@ const menuItems = [
   },
 ] as const;
 
-function FooterComponent() {
+export function Footer() {
   const pathname = usePathname();
 
   return (
     <footer className="fixed bottom-0 left-0 w-full glass-light backdrop-blur-[20px] border-t border-neon-blue/20 z-50 animate-fade-in-up">
       <div className="container mx-auto px-4 min-w-80 overflow-x-auto">
         <div className="flex justify-between items-center py-3 gap-7">
-          {menuItems.map((item, index) => (
+          {menuItems.map((item) => (
             <NeonButton
-              key={index}
+              key={item.url}
               variant={pathname === item.url ? "blue" : "ghost"}
               className="border-0 lg:border border-neon-purple/10"
             >
@@ -91,5 +90,3 @@ function FooterComponent() {
     </footer>
   );
 }
-
-export const Footer = memo(FooterComponent);
