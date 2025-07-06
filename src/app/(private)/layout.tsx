bc3a86d64ad26b9cbd4e3f44f0806e2d668a6cfd
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-07-06 19:48:29 -0300

    feat: create Layout private

---
+++ src/app/(private)/layout.tsx
@@ -0,0 +1,14 @@
+import { Footer } from "./views/footer/footer.view";
+
+export default function LayoutPrivate({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="container w-full h-full flex flex-col gap-4 p-1 pb-20 sm:p-4 sm:pb-20">
+      {children}
+      <Footer />
+    </div>
+  );
+}
