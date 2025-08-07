1393e09dfd7ec83ea811d8adea0365922090f909
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-08-06 22:22:28 -0300

    chore: apply return from to execute and to create output type

--- src/service/auth/recoverPassword.service.ts
+++ src/service/auth/recoverPassword.service.ts
@@ -1,17 +1,17 @@
 import { BASE_API_PATHS } from "@/infra/constants/baseApiPaths.constants";
-import { AuthDTO } from "@/domain/auth/auth.dto";
 import { Service } from "../service";
 import { HttpGateway } from "@/domain/http/http.gateway";
 
 type InputDTO = { email: string };
 
+type OutputDTO = { data: null; status: number };
 export class RecoverPasswordService
-  implements Service<InputDTO, Promise<void>>
+  implements Service<InputDTO, Promise<OutputDTO>>
 {
   constructor(private readonly http: HttpGateway["post"]) {}
 
-  async execute({ email }: InputDTO): Promise<void> {
-    await this.http<AuthDTO>(
+  async execute({ email }: InputDTO): Promise<OutputDTO> {
+    return await this.http<OutputDTO>(
       BASE_API_PATHS.AUTH.recovery_password,
       {
         email,
