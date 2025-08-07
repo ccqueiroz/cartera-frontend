32859d82a4fa284acd99cc149ec3ead82b31aa89
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-08-07 15:15:48 -0300

    chore: created updateBillsPayableMonth usecase test

--- src/usecase/bill/updateBillsPayableMonth.usecase.ts
+++ src/usecase/bill/updateBillsPayableMonth.usecase.ts
@@ -3,6 +3,7 @@ import { Usecase } from "../usecase";
 import { BillDTO } from "@/domain/bill/bill.dto";
 import { HandleResponseGateway } from "@/domain/core/api/handleResponse.gateway";
 import { UpdateBillPayableMonthService } from "@/service/bill/updateBillsPayableMonth.service";
+import { DomainMessageList } from "@/domain/core/constants/domainMessageList.constants";
 
 type InputPayload = Required<Pick<BillDTO, "id" | "payOut">> & {
   includePaymentDate: Date | undefined;
@@ -25,7 +26,11 @@ export class UpdateBillPayableMonthUseCase
     signal,
   }: InputDTO): Promise<HandleResponseDTO<BillDTO>> {
     if (!payload.id) {
-      return { success: false, error: "" };
+      return {
+        success: false,
+        error: DomainMessageList.BILL_ID_NOT_FOUND,
+        status: 400,
+      };
     }
 
     let payDate: number | null = null;
