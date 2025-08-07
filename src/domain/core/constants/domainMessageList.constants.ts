c7a6f9c8a724a4b0943c70b97e9af7be387ed25d
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-08-07 15:15:22 -0300

    chore: included ERROR_RENEWING_SESSION, SUCCESS_REGISTERED_ACCOUNT, BILL_ID_NOT_FOUND

--- src/domain/core/constants/domainMessageList.constants.ts
+++ src/domain/core/constants/domainMessageList.constants.ts
@@ -7,7 +7,9 @@ export const DomainMessageList = {
   COOKIES_NOT_DEFINED: "Cookies Store não está definida!",
   OPERATION_SUCCESS: "Operação realizada com sucesso!",
   TOKEN_MISSING: "Token de autorização inexistente!",
-  ERROR_RENEWING_SESSION: "Erro ao renovar a autorização! Faça login novamente!",
+  ERROR_RENEWING_SESSION:
+    "Erro ao renovar a autorização! Faça login novamente!",
   FAILED_TO_REFRESH_SESSION: "Falha ao revalidar a autorização!",
-  SUCCESS_REGISTERED_ACCOUNT: 'Conta criada com sucesso!'
+  SUCCESS_REGISTERED_ACCOUNT: "Conta criada com sucesso!",
+  BILL_ID_NOT_FOUND: "Id da Conta não identificado. Tente novamente!",
 } as const;
