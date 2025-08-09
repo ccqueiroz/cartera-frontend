15027f691e4e0478c6671cc63638d2cc50d89fd9
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-08-09 11:22:47 -0300

    refactor: refactor component to include AnalysisPeriod

--- src/app/(private)/components/filterSearchReportsByPeriod/filterSearchReportsByPeriod.component.tsx
+++ src/app/(private)/components/filterSearchReportsByPeriod/filterSearchReportsByPeriod.component.tsx
@@ -1,13 +1,16 @@
 import { DatePicker } from "@/app/components/core/datePicker/datePicker.component";
 import { NeonButton } from "@/app/components/core/neonButton/neonButton.component";
-
+import { AnalysisPeriod } from "./components/analysisPeriod/analysisPeriod.component";
+import { Calendar } from "lucide-react";
 
 export const FilterSearchReportsByPeriod = () => {
   return (
-    <div className="flex flex-col-reverse sm:flex-row items-center gap-3 mt-3 md:mt-0">
-      <DatePicker mode="range" />
-
-      <NeonButton variant="blue" className="w-full">Nova Transação</NeonButton>
+    <div className="flex flex-col md:flex-row items-center gap-4 mt-3 md:mt-0">
+      <div className="min-w-max flex gap-2">
+        <Calendar className="h-5 w-5 text-neon-blue" />
+        <span className="text-muted/80 font-medium">Período de análise:</span>
+      </div>
+      <AnalysisPeriod />
     </div>
   );
 };
