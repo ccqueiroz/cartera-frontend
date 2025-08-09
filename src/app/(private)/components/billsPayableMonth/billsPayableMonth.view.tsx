df4bbf85545c6d3ccca526889cd169b61f531749
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-08-09 11:20:16 -0300

    chore: change text color from component title and added Dolllar icon

--- src/app/(private)/components/billsPayableMonth/billsPayableMonth.view.tsx
+++ src/app/(private)/components/billsPayableMonth/billsPayableMonth.view.tsx
@@ -1,6 +1,7 @@
 import { GlassCard } from "@/app/components/core/glassCard/glassCard.component";
 import { ListBillsPayableMonthViewProps } from "./billsPayableMonth.types";
 import { InfiniteScrollBillsCardPayableMonth } from "./components/infiniteScrollBillsCardPayableMonth/infiniteScrollBillsCardPayableMonth.component";
+import { DollarSign } from "lucide-react";
 
 export function BillsPayableMonthView({
   fetchNextPage,
@@ -9,12 +10,17 @@ export function BillsPayableMonthView({
 }: ListBillsPayableMonthViewProps) {
   return (
     <GlassCard
-      className="p-5 h-[500px] overflow-hidden"
-      variant="blue"
+      className="p-5 h-[500px] overflow-hidden glass-card-hover border border-neon-lavender/20"
+      variant="dark"
       role="region"
       aria-label="Seção de contas a pagar do mês"
     >
-      <h2 className="mb-5">Contas a Pagar do Mês</h2>
+      <div className="mb-5 w-full flex gap-1 items-center">
+        <DollarSign className="h-5 w-5 text-neon-lavender" />
+        <h2 className="font-semibold text-neon-lavender">
+          Contas a Pagar do Mês
+        </h2>
+      </div>
       <InfiniteScrollBillsCardPayableMonth
         hasNextPage={hasNextPage}
         error={error}
