fac24464907d6700fa645e70faf7c3cf266c6ea0
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-08-09 11:15:45 -0300

    chore: included instruction to resolve nextNavigation in storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,6 +6,20 @@ const config: StorybookConfig = {
   framework: "@storybook/nextjs",
   staticDirs: [],
   features: { backgroundsStoryGlobals: false },
+  webpackFinal: async (config) => {
+    config.resolve = config.resolve || {
+      alias: {},
+      extensions: [],
+      fallback: {},
+      modules: [],
+    };
+
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "next/navigation": require.resolve("./mocks/nextNavigationMock.ts"),
+    };
+    return config;
+  },
 };
 
 export default config;
